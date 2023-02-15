d4b027f05ebebf132aa036e4ace2f32825354ec3
peter varga <pkvarg@yahoo.se>
2023-02-15 16:58:43 +0100

    done

--- jsm-ecommerce/pages/index.js
+++ jsm-ecommerce/pages/index.js
@@ -5,7 +5,6 @@ import { Product, FooterBanner, HeroBanner } from '../components'
 const Hero = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-    {console.log(bannerData)}
     <div className='products-heading'>
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
